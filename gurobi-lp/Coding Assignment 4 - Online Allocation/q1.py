8abb6a3e8c877f65eb80358d2df83644fcb1adba
syht <trongsyctk@gmail.com>
2018-08-07 21:47:50 +0700

    add file

--- gurobi-lp/Coding Assignment 4 - Online Allocation/q1.py
+++ gurobi-lp/Coding Assignment 4 - Online Allocation/q1.py
@@ -3,8 +3,15 @@ def q1():
     data = pd.read_csv("data_online_allocation.csv")
     #Your code goes here
     
-    #Construct A (has to be lists within a list)
+    m = 4
+    N = 100
     
+    #Construct A (has to be lists within a list)
+    A = []
+    A.append([1, 0, 0, 0])
+    A.append([0, 1, 0, 0])
+    A.append([0, 0, 1, 0])
+    A.append([0, 0, 0, 1])
     
     #Construct b (this will be the upper bound limit for your x1,..,x100
     
