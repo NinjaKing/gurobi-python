068cafc4f579e7eaa3cc08890fecb9bdbbcaee4f
syht <trongsyctk@gmail.com>
2018-08-11 17:05:05 +0700

    Add

--- gurobi-lp/Coding Assignment 5_Matching Problem/your_code.py
+++ gurobi-lp/Coding Assignment 5_Matching Problem/your_code.py
@@ -1,5 +1,5 @@
 def compute_optimal_allocation():
-    import gurobipy
+    from gurobipy import GRB, Model, quicksum
     import numpy
     from construct_lp_model import construct_lp_model
  
@@ -13,32 +13,60 @@ def compute_optimal_allocation():
  
  
     # Your code goes here
+    m = Model('matching')
 
-    model = construct_lp_model(c, A, b, B, d)
-    model.optimize()
+    # Your code goes here
+    n_jobs = cost.shape[0]
+    n_servers = cost.shape[1]
+    
+    # The following example is here to help you build the model (you must adapt it !!!) 
+    # Define decision variables
+    x = []
+    for i in range(n_jobs):
+        x_i = []
+        for j in range(n_servers):
+            x_i.append(m.addVar(vtype=GRB.BINARY, name = 'var_{}_{}'.format(i, j)))
+        x.append(x_i)
+    m.update()
+
+    # Define constraints:
+    # each job only assigned to 1 server
+    for i in range(n_jobs):
+        lhs = LinExpr()
+        for j in range(n_servers):
+            expr.add(x[i][j], 1)
+        m.addConstr(lhs, GRB.EQUAL, rhs=1)  
+    m.update()
+    
+    # each server can do at most 2 jobs
+    for j in range(n_servers):
+        lhs = LinExpr()
+        for i in range(n_jobs):
+            expr.add(x[i][j], 1)
+        m.addConstr(lhs, '<=', rhs=2)  
+    m.update()
+ 
+    # Define objective function:
+    objexpr = LinExpr()
+    for i in range(n_jobs):
+        for j in range(n_servers):
+            objexpr.add(x[i][j], cost[i, j])
+    m.setObjective(objexpr, GRB.MINIMIZE)
+    m.update()
+    
+    m.optimize()
+ 
+    total_cost = m.objVal
+    optimal_allocation = []
+    for i in range(n_jobs):
+        for j in range(n_servers):
+            if x[i][j] == 1:
+                optimal_allocation.append((i + 1, j + 1))
+    model = v
 
     # Your code goes here
  
- 
-
-
-
- cost = np.array([[17.8, 16.96, 13.56, 12.22, 15.88, 17.59], 
-                 [13.11, 7.14,  8.57, 8.67,  7.23, 14.49],
-                 [12.62, 9.1 ,  8.97, 7.75, 16.71, 16.59],
-                 [12.87, 7.14,  9.75, 13.87, 13.59, 12.37],
-                 [17.92, 14.5 , 14.91, 10.0 , 13.67, 12.56], 
-                 [9.9 , 15.7 , 15.32, 16.8 , 17.34, 18.21]])
- c = - cost.flatten()
- b = np.array([1 for _ in range(6)])
- d = np.array([2 for _ in range(6)])
-
- # Construction of the matrix A by blocks
- A = [[0 for _ in range(6 * i)] + [1 for _ in range(6)] + [0 for _ in range(6*(i+1), 6 * 6)] for i in range(6)]
- A = np.array(A)
- print(A, b, c, d)
- 
- # optimal_cost = optimal total cost for the company
- # optimal_allocation = see instructions provided in the problem description on edX
- # model = gurobi model returned by the lp_standard_form function
- return([total_cost, optimal_allocation, model])
+    # optimal_cost = optimal total cost for the company
+    # optimal_allocation = see instructions provided in the problem description on edX
+    # model = gurobi model returned by the lp_standard_form function
+    return ([total_cost, optimal_allocation, model])
